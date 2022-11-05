344a8f1b59189a27553514d9b072ae8ef4581dd6
cristo_ <cristiano.henriqueferreirabp@gmail.com>
2022-11-05 19:23:49 -0300

    initial commit

---
+++ src/main/java/com/architectus/architectus/ArchitectusApplication.java
@@ -0,0 +1,13 @@
+package com.architectus.architectus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectusApplication.class, args);
+	}
+
+}
